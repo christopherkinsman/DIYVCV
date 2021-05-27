774727b09987500976e065308c0ebd3ba0325c14
Chris Kinsman <chris.t.kinsman@gmail.com>
2021-05-27 11:54:58 -0400

    updates CSS

--- DIYVCV/Controllers/ComponentController.cs
+++ DIYVCV/Controllers/ComponentController.cs
@@ -25,9 +25,10 @@ namespace DIYVCV.Controllers
             };
             client = new HttpClient(handler);
             //change this to match your own local port number
-            client.BaseAddress = new Uri("https://localhost:44300/api/");
+            client.BaseAddress = new Uri("http://diyvcv-env.eba-pza4kszm.us-east-2.elasticbeanstalk.com/api/");
+            //client.BaseAddress = new Uri("https://localhost:44300/api/");
             client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json")); 
+            new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         /// <summary>
--- DIYVCV/Models/DIYVCVDataContext.cs
+++ DIYVCV/Models/DIYVCVDataContext.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+//using Microsoft.AspNet.Identity.EntityFramework;
+
 
 namespace DIYVCV.Models
 {
     public class DIYVCVDataContext : DbContext
     {
-        public DIYVCVDataContext() : base("name=DIYVCVDataContext")
+/*        public DIYVCVDataContext() : base("name=DIYVCVDataContext")
         {
 
+        }*/
+
+        /* AWS Connection*/
+        public DIYVCVDataContext()
+            : base(AWSConnector.GetRDSConnectionString())
+        {
         }
 
         public DbSet<Module> Modules { get; set; }
